20e7f890570862b90a91d70934c111e12c5c2133
WhiteBlackGoose <wbg@angouri.org>
2022-11-30 22:03:17 +0300

    upd

--- playground.py
+++ playground.py
@@ -5,5 +5,16 @@ traders = []
 traders += [Random(xg, cash=1000) for i in range(10)]
 traders += [Fundamentalist(xg, cash=1000) for i in range(10)]
 traders += [Chartist(xg, cash=1000) for i in range(10)]
-sim = Simulator(exchange=ExchangeAgent(), traders=traders)
-sim.simulate(100)
+
+sim = Simulator(exchange=xg, traders=traders)
+
+sim.simulate(100000)
+
+sim.info.prices
+import numpy as np
+np.std(np.array(sim.info.prices))
+sim.info.orders
+
+import matplotlib.pyplot as plt
+plt.plot(np.array(sim.info.prices) + 222)
+plt.savefig('/tmp/out.png')
